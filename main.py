1c8fb3a76c0b653dea530bb1a35d2da3ee7f249a
Shane Perreault <shaneprrlt@gmail.com>
2017-12-20 09:14:55 -0600

    improved kaggle score to 0.7655! want to do better still

--- main.py
+++ main.py
@@ -2,71 +2,61 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
 from sklearn.ensemble import RandomForestClassifier
-import pdb
 
-## Load in Training Data and Testing Data
-train_data = pd.read_csv('data/train.csv')
-test_data = pd.read_csv('data/test.csv')
+## load in titanic data
+data = pd.read_csv('data/train.csv')
+eval_data = pd.read_csv('data/test.csv')
 
-## Drop uncessary columns
-train_data = train_data.drop(['PassengerId', 'Name', 'Ticket'], axis=1)
-test_data = test_data.drop(['Name', 'Ticket'], axis=1)
+def clean_data(data=[]):
+    ## clean up data by dropping unremarkable columns,
+    ## filling null data, and converting categorical data
+    ## into numerical values
+    data = data.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+    data['Embarked'] = data['Embarked'].fillna('S')
+    data['Age'] = data['Age'].fillna(data['Age'].median())
+    data['Fare'] = data['Fare'].fillna(data['Fare'].median())
+    data = pd.get_dummies(data, columns=['Sex', 'Embarked'])
+    return data
 
-## Clean up NaN Data
-train_data['Embarked'] = train_data['Embarked'].fillna('S')
-test_data['Embarked'] = test_data['Embarked'].fillna('S')
+data = clean_data(data)
 
-# ## Display Histogram of Ages
-# data.Age.hist()
-# plt.show()
-#
-# ## Find a correlation in the data
-# corr = data.corr()
-# fig, ax = plt.subplots(figsize=(16, 12))
-# fig = sns.heatmap(corr,annot=True)
-# plt.show()
-#
-# ## plot embarked vs survived
-# sns.factorplot('Embarked', 'Survived', data=train_data, size=4, aspect=3)
-# plt.show()
-#
-# ## plot age vs survived
-# sns.factorplot('Age', 'Survived', data=train_data, size=4, aspect=3)
-# plt.show()
-#
-# ## plot pclass vs survived
-# sns.factorplot('Pclass', 'Survived', data=train_data, size=4, aspect=3)
-# plt.show()
+## split into training and test groups
+split_idx = math.ceil(len(data) * .8)
+train_data = data[:split_idx]
+test_data = data[split_idx:]
 
-train_data.drop(['Embarked', 'Cabin'], axis=1, inplace=True)
-test_data.drop(['Embarked', 'Cabin'], axis=1, inplace=True)
-
-# def get_person(passenger):
-#     age,sex = passenger
-#     return 'child' if age < 16 else sex
-#
-# train_data['Person'] = train_data[['Age', 'Sex']].apply(get_person, axis=1)
-# test_data['Person'] = test_data[['Age', 'Sex']].apply(get_person, axis=1)
-
-train_data.drop(['Age', 'Sex', 'Parch', 'SibSp'], axis=1, inplace=True)
-test_data.drop(['Age', 'Sex', 'Parch', 'SibSp'], axis=1, inplace=True)
-
-train_data['Fare'].fillna(train_data['Fare'].median(), inplace=True)
-test_data['Fare'].fillna(test_data['Fare'].median(), inplace=True)
-
+## Split into features & label sets
 X_train = train_data.drop(['Survived'], axis=1)
 y_train = train_data['Survived']
-X_test = test_data.drop("PassengerId", axis=1).copy()
 
-random_forest = RandomForestClassifier(n_estimators=100)
+X_test = test_data.drop(['Survived'], axis=1)
+y_test = test_data['Survived']
+
+## train a random forest classifier
+random_forest = RandomForestClassifier(n_estimators=50, max_features=10, max_depth=5)
 random_forest.fit(X_train, y_train)
-Y_pred = random_forest.predict(X_test)
-score = random_forest.score(X_train, y_train)
+score = random_forest.score(X_test, y_test)
+print('Score: %s'%score)
 
+## create predictions off of evaluation data
+X_eval = clean_data(eval_data)
+Y_eval = random_forest.predict(X_eval)
 results = pd.DataFrame.from_dict({
-    "PassengerId": test_data['PassengerId'],
-    "Survived": Y_pred
+    "PassengerId": eval_data['PassengerId'],
+    "Survived": Y_eval
 })
-
 results.to_csv('data/results.csv', index=False)
+
+# random_forest = RandomForestClassifier(n_estimators=100)
+# random_forest.fit(X_train, y_train)
+# Y_pred = random_forest.predict(X_test)
+# score = random_forest.score(X_train, y_train)
+#
+# results = pd.DataFrame.from_dict({
+#     "PassengerId": test_data['PassengerId'],
+#     "Survived": Y_pred
+# })
+#
+# results.to_csv('data/results.csv', index=False)
